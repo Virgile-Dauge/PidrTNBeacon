c7c5f9ae0fb5229475021c0c1afb45b10f965aca
virgile Daugé <virgile.dauge@telecomnancy.net>
2015-05-26 19:57:51 +0200

    nettoyage

--- app/src/main/java/fr/virgiledauge/tnbeacon/MainActivity.java
+++ app/src/main/java/fr/virgiledauge/tnbeacon/MainActivity.java
@@ -18,7 +18,6 @@ Imports divers
  */
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.PointF;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
